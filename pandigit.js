c57ed1bb7cd39c4d299e679dd424a81e7fa59744
Michelle Jiang <mhjiang@smith.edu>
2025-10-25 02:36:32 -0400

    improved efficiency using every

--- pandigit.js
+++ pandigit.js
@@ -1,25 +1,11 @@
 //A pandigital number contains all digits (0-9) at least once. Write a function that takes an integer, returning true if the integer is pandigital, and false otherwise.
 
 function pandigital(n) { 
+    
     let n_array = n.toString().split("").map(Number); // converts integer to array of digits 
     let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    if (n_array.length >= 10) {
-        if (n_array.includes(0) 
-            && n_array.includes(1) 
-            && n_array.includes(2) 
-            && n_array.includes(3) 
-            && n_array.includes(4)
-            && n_array.includes(5)
-            && n_array.includes(6)
-            && n_array.includes(7)
-            && n_array.includes(8)
-            && n_array.includes(9)
-         ) { 
-            return true; 
-         }
-    } else { 
-        return false; 
-    }
+    return digits.every(d => n_array.includes(d)) // returns true if n_array contains every value of digits & false otherwise
 }
 
 console.log(pandigital(120)); 
+console.log(pandigital(543126789012))
