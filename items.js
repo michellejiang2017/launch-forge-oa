58bbff74c2e2e87c25a634a366377fdfeab0ff3a
Michelle Jiang <mhjiang@smith.edu>
2025-10-26 14:54:03 -0400

    initial ideas

---
+++ items.js
@@ -0,0 +1,39 @@
+class items { 
+    let title = ""; 
+    let description = ""; 
+    let dateCreated = ""; //should it be a string? 
+    let dateDue = ""; 
+    let statusList = ["New", "Working on", "Finished"]; // maybe it can be a list or like a selection? 
+    let status = statusList[0]; 
+
+    function getTitle() { 
+
+    }
+
+    function getDescription() { 
+
+    }
+
+    function getDateCreated() { 
+
+    }
+
+    function getDateDue() { 
+
+    }
+
+    function daysLeft() { 
+        getDateDue - getDateCreated //something like that 
+    }
+    function getStatus() { 
+
+    }
+
+    function setTitle() { 
+
+    }
+
+    // setters for everything 
+
+    // constructor that sets everything? 
+}
---
+++ todo.js
@@ -0,0 +1,40 @@
+/*
+Question 1:
+My brand new startup wants to build a better online productivity tracker. We think that current trackers are far too complicated, and we want to make something that is a simple to-do list.
+
+Without worrying about design/HTML, write the functions needed to make this to-do list workable.
+
+Write the JavaScript functions to make this to-do list functional. Please do not write any HTML or use frameworks like jQuery. The list should be saved as an array of objects. Imagine each piece of functionality would be triggered by a button that called the corresponding function.
+
+The requirements for the to do list are:
+To Do items should have properties of title, description, date created, date due, and its status (New, Working on, Finished).
+The ability to add and delete items
+The ability to reorganize the list (i.e. bring one to top, send down 1, etc)
+The ability to edit information about each task where appropriate
+Any additional functionality you can think of is highly encouraged, but keep in mind the to do list is supposed to be simple.
+*/
+
+import * from items; 
+
+class todo { 
+
+    // attributes 
+    let items toDo = []; //list of to do items
+    // so to do is a list of objects so you need to create an object that is like the item 
+
+    function addNew() { 
+
+    }
+    
+    function remove() { 
+
+    }
+
+    function reorganize() { 
+
+    }
+
+    function editItem() { 
+        // setter and getter
+    }
+}
