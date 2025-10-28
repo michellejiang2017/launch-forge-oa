323186cf584424c6db1bdff5719006b134b4da24
Michelle Jiang <mhjiang@smith.edu>
2025-10-28 02:29:41 -0400

    set up constructor & worked on methods

--- items.js
+++ items.js
@@ -1,10 +1,29 @@
 class items { 
+    /*
     let title = ""; 
     let description = ""; 
     let dateCreated = ""; //should it be a string? 
     let dateDue = ""; 
     let statusList = ["New", "Working on", "Finished"]; // maybe it can be a list or like a selection? 
-    let status = statusList[0]; 
+    let status = statusList[0]; */
+
+    constructor(title, description, dateDue) { 
+        this.title = title; 
+        this.description = description; 
+        this.dateCreated = new Date(); 
+        //if dateDue = regex
+        this.dateDue = new Date(dateDue); 
+        this.status = "New"; 
+    }
+    
+
+    function setStatus(dateCreated, dateDue) { 
+        if (dateDue > dateCreated) { 
+            this.status = "Working on"; 
+        } else if (dateDue <= dateCreated) {
+            this.status = "Finished"
+        }
+    }
 
     function getTitle() { 
 
